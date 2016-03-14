eb94a9a7c171712ef255b792077ed82ca888a2aa
Ivan Orlov <orlov_mail@mail.ru>
2016-03-14 04:14:42 +0300

    composer.json

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
-# yii2-outlog
+Yii2-outlog
+===================
 Yii2 component for submiting errors to outer logging system
+
+Installation
+---
+Run
+```code
+php composer.phar require "demi/yii2-outlog" "~1.0"
+```
+or
+
+
+Add to composer.json in your project
+```json
+{
+	"require": {
+  		"demi/yii2-outlog": "~1.0"
+	}
+}
+```
+then run command
+```code
+php composer.phar update
+```
+
+# Configurations
+---
+
---
+++ WebErrorHandler.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace demi\yii2outlog;
+
+use Yii;
+
+/**
+ * Handles exceptions in web applications
+ */
+class WebErrorHandler extends \yii\web\ErrorHandler
+{
+    /**
+     * Tracks if we are in the exception handler and have already notified Bugsnag about
+     * the exception
+     *
+     * @var boolean
+     */
+    protected $inExceptionHandler = false;
+
+    /**
+     * Only log the exception here if we haven't handled it below (in handleException)
+     */
+    public function logException($exception)
+    {
+        if (!$this->inExceptionHandler) {
+            Yii::$app->bugsnag->notifyException($exception);
+        }
+        try {
+            Yii::error("Caught exception " . get_class($exception) . ": " . (string)$exception,
+                BugsnagComponent::IGNORED_LOG_CATEGORY);
+        } catch (\Exception $e) {
+        }
+    }
+
+    /**
+     * Ensures CB logs are written to the DB if an exception occurs
+     */
+    public function handleException($exception)
+    {
+        Yii::$app->bugsnag->notifyException($exception);
+        $this->inExceptionHandler = true;
+        parent::handleException($exception);
+    }
+
+    /**
+     * Handles fatal PHP errors
+     */
+    public function handleFatalError()
+    {
+        // When running under codeception, a Yii application won't actually exist, so we just have to eat it here...
+        if (is_object(Yii::$app)) {
+            // Call into Bugsnag client's errorhandler since this will potentially kill the script below
+            Yii::$app->bugsnag->runShutdownHandler();
+        }
+        parent::handleFatalError();
+    }
+}
